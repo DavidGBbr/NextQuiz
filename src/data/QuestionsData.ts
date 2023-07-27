d8e83e28a4ba73d1b7d94538a50a77be250603c0
davidGBbr <davidgomesbrigido@gmail.com>
2023-07-27 20:30:21 -0300

    structure created

---
+++ src/data/QuestionsData.ts
@@ -0,0 +1,9 @@
+import { Question } from "@/types/Question";
+
+export const QuestionsData: Question[] = [
+  {
+    question: "Qual ingrediente não pode faltar?",
+    options: ["Farinha", "Corante", "Água", "Açúcar"],
+    answer: 1,
+  },
+];
